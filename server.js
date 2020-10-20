4b92800271a8e0ec9f627bff1ebeba399ab841a8
keelisetti lokesh <keelisettilokesh@gmail.com>
2020-10-20 14:35:16 +0530

    added a post method for complaint and changes in student route

--- server.js
+++ server.js
@@ -5,22 +5,19 @@ const bodyParser = require('body-parser');
 const db = require('./db'); //database connection for queries
 
 
-
 var indexRouter = require('./routes/index'); //routes defined in other files
 var studentRouter=require('./routes/student');
-
 
 // var passport = require('passport');
 // var GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
 
 // passport.use(
-// 	new GoogleStrategy({
+// new GoogleStrategy({
 
-// 	}),()=>{
+// }),()=>{
 
-// 	}
+// }
 // )
-
 
 var port = process.env.PORT || 3000;
 
@@ -46,39 +43,10 @@ app.listen(port, () => console.log(`listening to PORT ${port}...`));
 app.use('/',indexRouter); //router for home page before login
 
 app.get('/login',(req,res)=>{
-	res.render('login.ejs');
+res.render('login.ejs');
 })
 
 app.use('/student',studentRouter); // route for studentpage
 
+
 module.exports = app;
-=======
-app.get('/complaint',(req,res)=>{
-	var id = req.query.id;
-	var query = 'SELECT complaint_list.complaint_id,complaint_subject,complaint_text,complaint_list.date,dept_name,reply_text,reply_list.date,from_to FROM ((complaint_list INNER JOIN department_list ON complaint_list.dept_id = department_list.dept_id) INNER JOIN reply_list ON reply_list.complaint_id = complaint_list.complaint_id and complaint_list.complaint_id = '+id+');' ;
-	db.query(query, function (err, result, fields) {
-	if (err) throw err;
-	    console.log(result);
-	    res.render('complaint.ejs',{result:result});
-	});
-})
-
-app.get('/student_home',(req,res)=>{
-	var studentId = 2;
-	var query = "SELECT complaint_id,complaint_subject,date,dept_name FROM complaint_list INNER JOIN department_list ON department_list.dept_id = complaint_list.dept_id and student_id = "+studentId+" and resolved = 0;"
-	db.query(query, function (err, result, fields) {
-	if (err) throw err;
-	    console.log(result);
-	    res.render('student_home.ejs',{result:result});
-	});
-})
-
-app.get('/student_history',(req,res)=>{
-	var studentId = 2;
-	var query = "SELECT complaint_id,complaint_subject,date,dept_name FROM complaint_list INNER JOIN department_list ON department_list.dept_id = complaint_list.dept_id and student_id = "+studentId+" and resolved = 1;"
-	db.query(query, function (err, result, fields) {
-	if (err) throw err;
-	    console.log(result);
-	    res.render('student_history.ejs',{result:result});
-	});
-})
