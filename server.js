7c2f0e0354bc122437009006f3523e1dca4eed0d
Kushaan Gowda <kushaangowda@yahoo.com>
2020-10-20 08:24:18 +0530

    implemented student_home and student_history

--- server.js
+++ server.js
@@ -3,17 +3,6 @@ const logger = require('morgan');
 const ejs = require('ejs');
 const bodyParser = require('body-parser');
 const db = require('./db'); //database connection for queries
-// var passport = require('passport');
-// var GoogleStrategy = require('passport-google-oauth').OAuthStrategy;
-
-// passport.use(
-// 	new GoogleStrategy({
-
-// 	}),()=>{
-
-// 	}
-// )
-
 
 var port = process.env.PORT || 3000;
 
@@ -50,10 +39,6 @@ app.get('/login',(req,res)=>{
 	res.render('login.ejs');
 })
 
-app.get('/student_home',(req,res)=>{
-	res.render('student_home.ejs');
-})
-
 app.get('/complaint',(req,res)=>{
 	var id = req.query.id;
 	var query = 'SELECT complaint_list.complaint_id,complaint_subject,complaint_text,complaint_list.date,dept_name,reply_text,reply_list.date,from_to FROM ((complaint_list INNER JOIN department_list ON complaint_list.dept_id = department_list.dept_id) INNER JOIN reply_list ON reply_list.complaint_id = complaint_list.complaint_id and complaint_list.complaint_id = '+id+');' ;
@@ -64,3 +49,22 @@ app.get('/complaint',(req,res)=>{
 	});
 })
 
+app.get('/student_home',(req,res)=>{
+	var studentId = 2;
+	var query = "SELECT complaint_id,complaint_subject,date,dept_name FROM complaint_list INNER JOIN department_list ON department_list.dept_id = complaint_list.dept_id and student_id = "+studentId+" and resolved = 0;"
+	db.query(query, function (err, result, fields) {
+	if (err) throw err;
+	    console.log(result);
+	    res.render('student_home.ejs',{result:result});
+	});
+})
+
+app.get('/student_history',(req,res)=>{
+	var studentId = 2;
+	var query = "SELECT complaint_id,complaint_subject,date,dept_name FROM complaint_list INNER JOIN department_list ON department_list.dept_id = complaint_list.dept_id and student_id = "+studentId+" and resolved = 1;"
+	db.query(query, function (err, result, fields) {
+	if (err) throw err;
+	    console.log(result);
+	    res.render('student_history.ejs',{result:result});
+	});
+})
