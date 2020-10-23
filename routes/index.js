414f54a54ca4a6be7c88ddf16fd12a8195e4fcd2
Kushaan Gowda <kushaangowda@yahoo.com>
2020-10-23 21:46:26 +0530

    made code simpler

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,21 @@ var express = require('express');
 var router = express.Router();
 var db = require('../db');
 
+function checkError(err,res){
+	if(err){
+		console.log(err);
+		res.send({success:false,message:'database error',err:err});
+	}
+}
+
 router.get('/',(req,res)=>{
 	//Select all customers and return the result object:
 	var query = "SELECT complaint_id,complaint_subject,date,dept_name FROM complaint_list \
 				INNER JOIN department_list ON department_list.dept_id = complaint_list.dept_id order by date desc;"
 	db.query(query, function (err, result, fields) {
-	if (err){
-		console.log(err);
-		res.send({success:false,message:'database error',err:err});	
-	}else{
+		checkError(err,res)
 		console.log(result);
 	    res.render('index.ejs',{result:result});
-	}
 	});
 })
 
@@ -23,13 +26,9 @@ router.get('/complaint',(req,res)=>{
 				 FROM ((complaint_list INNER JOIN department_list ON complaint_list.dept_id = department_list.dept_id and complaint_id = '+id+') \
 				 LEFT JOIN reply_list ON reply_list.complaint_id = '+id+');' ;
 	db.query(query, function (err, result, fields) {
-	if (err){
-		console.log(err);
-		res.send({success:false,message:'database error',err:err});	
-	}else{
+		checkError(err,res)
 		console.log(result);
 	    res.render('complaint.ejs',{result:result,role:'general'});
-	}
 	});
 })
 
